1a56c3d3415db42c6d45cefd534d8b1e5449ad7c
maiductrung99 <maiductrung99@gmail.com>
2025-11-28 17:42:06 +0700

    + Change the path of Body Pose features

--- body_pose/openposeBodyCustom/download_models.sh
+++
@@ -1,2 +0,0 @@
-gdown 1ghXakEXhBMCdV78K6tCFTPp_vjJDWmcE -O /content/drive/MyDrive/Research/Computer_Vision/StudentEngagement/pytorch_openpose_body/model/body_25.pth
-gdown 1VPiIxXk5KWEwdJlVVe5PDQ1QufMS1Zpk -O /content/drive/MyDrive/Research/Computer_Vision/StudentEngagement/pytorch_openpose_body/model/body_coco.pth
